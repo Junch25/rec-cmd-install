729f28698a005613b874bff95f5bd7b166af83db
Junichi <junichi@JunichinoMacBook-Pro.local>
2022-10-17 23:15:41 +0900

    update

--- shell/px4driver-install.sh
+++ shell/px4driver-install.sh
@@ -2,19 +2,18 @@
 
 echo "Driver install start!"
 mkdir ~/git
-cd ~/git
-git clone https://github.com/nns779/px4_drv.git
-cd px4_drv/fwtool
+cd ~/git || exit
+git clone https://github.com/nns779/px4_drv.git && cd px4_drv/fwtool || exit
 make
 wget http://plex-net.co.jp/plex/pxw3u4/pxw3u4_BDA_ver1x64.zip -O pxw3u4_BDA_ver1x64.zip
 unzip -oj pxw3u4_BDA_ver1x64.zip pxw3u4_BDA_ver1x64/PXW3U4.sys
 ./fwtool PXW3U4.sys it930x-firmware.bin
 sudo mkdir -p /lib/firmware
 sudo cp it930x-firmware.bin /lib/firmware/
-cd ../driver
+cd ../driver || exit
 make
 sudo make install
-cd ../
+cd ../ || exit
 sudo cp -a ./ /usr/src/px4_drv-0.2.1
 sudo dkms add px4_drv/0.2.1
 sudo dkms install px4_drv/0.2.1
--- shell/recpt1-install.sh
+++ shell/recpt1-install.sh
@@ -2,17 +2,17 @@
 
 # libarib25 install
 echo "libarib25 and recpt1 install start!"
-cd ~/git
+cd ~/git || exit
 git clone https://github.com/stz2012/libarib25.git
-cd libarib25/
+cd libarib25/ || exit
 cmake .
 make
 sudo make install
 
 # recpt1 install
-cd ~/git
+cd ~/git || exit
 git clone https://github.com/stz2012/recpt1.git
-cd recpt1/recpt1
+cd recpt1/recpt1 || exit
 ./autogen.sh
 ./configure --enable-b25 
 make
